10533547f04aeba685b64b31e91285bfce801772
faiz1701 <thefaiz17@gmail.com>
2022-03-30 13:56:57 +0700

    add js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+gsap.from('.navbar', {duration: 2, y: -100, opacity: 0, ease: 'elastic' });
+
+gsap.from('.contact', {duration: 2, y: -100, opacity: 0, ease: 'elastic' });
+
+
+gsap.registerPlugin(TextPlugin);
